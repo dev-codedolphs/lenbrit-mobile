d2111f839e326c0ad17db25fa08fcebef26a53dd
muhammad waqas <mwaqas1757@gmail.com>
2025-04-29 02:33:03 +0500

    created mobile app

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Mobile repo for Waqas");
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
